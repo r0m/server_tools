75a109f0d5e644cce5762d4f3efabda94ef43e8f
r0m <rom@bifrost>
2015-09-03 07:17:27 -0400

    light modification on check_archive

--- check_archive.sh
+++ check_archive.sh
@@ -2,7 +2,7 @@
 
 MD5_FILE="md5sum.txt"
 BCKP_DIR=$HOME/backup_folder
-LIST_BACKUP_DIR=`find $HOME/backup_folder/ -name "$MD5_FILE" -exec dirname {} \;`
+LIST_BACKUP_DIR=`find "$BCKP_DIR"/ -name "$MD5_FILE" -exec dirname {} \;`
 
 for UN in $LIST_BACKUP_DIR
 do
